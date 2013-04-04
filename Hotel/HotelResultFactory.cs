14a948568ee54dffc0a267abf70a50eaea08e801
Andrew Moorcroft <moorcroftlad+git@hotmail.com>
2013-04-04 17:30:12 +0100

    hotel results page

--- Hotel/HotelResultFactory.cs
+++ Hotel/HotelResultFactory.cs
@@ -1,4 +1,5 @@
-﻿using Geography;
+﻿using System;
+using Geography;
 using LateRoomsScraper;
 
 namespace Results
@@ -22,7 +23,7 @@ namespace Results
                 {
                     Price = GetPrice(response),
                     Uri =
-                        "http://m.laterooms.com/en/p9827/MobileSearch.aspx?k=Manchester&d=20130404&n=1&adults=1&children=0&minp=&maxp=&StarRatingFilter=&rt=1-0&Latitude=&Longitude=&MaxRadius=1&PageSize=10&toStep=&SortBy=Price&Ascending=True&findButton=FIND+HOTELS&sb=tp&sd=true"
+                        string.Format("http://m.laterooms.com/en/p9827/MobileAjax.aspx?pageSize=10&search=%7B%22Latitude%22:{0},%22Longitude%22:{1},%22Radius%22:1,%22RadiusDistanceUnit%22:%22Miles%22,%22Date%22:%22{2:yyyy}{2:MM}{2:dd}%22,%22CurrencyId%22:%22GBP%22,%22HotelFilter%22:1,%22SortOrder%22:%22TotalPrice%22,%22SortedAscending%22:true,%22Type%22:%22Standard%22,%22PageNumber%22:1,%22Facilities%22:0,%22StarRating%22:0,%22StarRatingBitmap%22:0,%22CustomerRatingBitmap%22:0,%22AppealBitmap%22:0,%22CustomerRatingPercentageFrom%22:0,%22MinPrice%22:0,%22MaxPrice%22:99999999,%22HasSpecialOffers%22:false,%22SpecialOffersBitmap%22:0,%22Nights%22:1%7D", latitude, longitude, DateTime.Now)
                 };
             
             return hotelResult;
