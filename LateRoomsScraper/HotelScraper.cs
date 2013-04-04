fbc049094bb42258eb4033f5859c5fc511412ace
Andrew Moorcroft <moorcroftlad+git@hotmail.com>
2013-04-04 17:21:11 +0100

    Revert "hotel search results points to ajax results"
    This reverts commit 46ca8ccd127cb68670e20911866d723ba21b0213.

--- LateRoomsScraper/HotelScraper.cs
+++ LateRoomsScraper/HotelScraper.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Net;
 using HtmlAgilityPack;
 
@@ -7,19 +6,12 @@ namespace LateRoomsScraper
 {
     public class HotelScraper : IScrapeWebsites
     {
-        private const string UrlFormat = "http://m.laterooms.com/en/p9827/MobileAjax.aspx?pageSize=10&search=%7B%22Latitude%22:{0},%22Longitude%22:{1},%22Radius%22:1,%22RadiusDistanceUnit%22:%22Miles%22,%22Date%22:%22{2}%22,%22CurrencyId%22:%22GBP%22,%22HotelFilter%22:1,%22SortOrder%22:%22TotalPrice%22,%22SortedAscending%22:true,%22Type%22:%22Standard%22,%22PageNumber%22:1,%22Facilities%22:0,%22StarRating%22:0,%22StarRatingBitmap%22:0,%22CustomerRatingBitmap%22:0,%22AppealBitmap%22:0,%22CustomerRatingPercentageFrom%22:0,%22MinPrice%22:0,%22MaxPrice%22:99999999,%22HasSpecialOffers%22:false,%22SpecialOffersBitmap%22:0,%22Nights%22:1%7D";
-        private string Date
-        {
-            get
-            {
-                return string.Format("{0:yyyy}{0:MM}{0:dd}", DateTime.Now);
-            }
-        }
+        private const string UrlFormat = "http://m.laterooms.com/en/p9827/MobileAjax.aspx?pageSize=10&search=%7B%22Latitude%22:{0},%22Longitude%22:{1},%22Radius%22:1,%22RadiusDistanceUnit%22:%22Miles%22,%22Date%22:%2220130404%22,%22CurrencyId%22:%22GBP%22,%22HotelFilter%22:1,%22SortOrder%22:%22TotalPrice%22,%22SortedAscending%22:true,%22Type%22:%22Standard%22,%22PageNumber%22:1,%22Facilities%22:0,%22StarRating%22:0,%22StarRatingBitmap%22:0,%22CustomerRatingBitmap%22:0,%22AppealBitmap%22:0,%22CustomerRatingPercentageFrom%22:0,%22MinPrice%22:0,%22MaxPrice%22:99999999,%22HasSpecialOffers%22:false,%22SpecialOffersBitmap%22:0,%22Nights%22:1%7D";
         private string _latitude;
         private string _longitude;
         private string ScrapeUrl
         {
-            get { return string.Format(UrlFormat, _latitude, _longitude, Date); }
+            get { return string.Format(UrlFormat, _latitude, _longitude); }
         }
 
         public IScraperResponse Scrape(string latitude, string longitude)
