04d1a10afee7b680977b5f988d85da74488b87f9
Andrew Moorcroft <moorcroftlad+git@hotmail.com>
2013-04-04 08:49:20 +0100

    moved add2home to bundler, added google maps api

--- HotelOrTaxi/App_Start/BundleConfig.cs
+++ HotelOrTaxi/App_Start/BundleConfig.cs
@@ -7,7 +7,8 @@ namespace HotelOrTaxi.App_Start
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
-                "~/assets/js/bootstrap.js"));
+                "~/assets/js/bootstrap.js",
+                "~/assets/js/add2home.js"));
             bundles.Add(new StyleBundle("~/bundles/css").Include(
                 "~/assets/css/bootstrap-responsive.css",
                 "~/assets/css/style.css",
