245e5d8876806ff44a8c3752cd021c41c0589728
Andrew Moorcroft <moorcroftlad+git@hotmail.com>
2013-04-04 09:26:16 +0100

    attached autocomplete to to field

---
+++ HotelOrTaxi/assets/js/autocomplete.js
@@ -0,0 +1,13 @@
+﻿function initialize() {
+    var input = document.getElementById('to');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    google.maps.event.addListener(autocomplete, 'place_changed', function () {
+        var place = autocomplete.getPlace();
+
+        alert(autocomplete.getBounds());
+
+        $('to').val(place.name);
+    });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
