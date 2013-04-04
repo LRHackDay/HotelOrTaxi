c567d926bb4440f2724a8a91a8250847c9f7bac7
rubygem <gemma.cameron@gmail.com>
2013-04-04 12:46:57 +0100

    Got the controller generating the URI's now.

--- HotelOrTaxi.Tests/Controllers/TestResultsController.cs
+++ HotelOrTaxi.Tests/Controllers/TestResultsController.cs
@@ -6,12 +6,14 @@ using NUnit.Framework;
 namespace HotelOrTaxi.Tests.Controllers
 {
     [TestFixture]
-    public class TestResultsController
+    public class TestResultsController : ICreateTheTaxiControllerUri
     {
+        private string _taxiUri;
+
         [Test]
         public void DisplaysIndex()
         {
-            var resultsController = new ResultsController();
+            var resultsController = new ResultsController(this);
             ViewResult viewResult = resultsController.Index(null, null, null, null);
 
             string viewName = viewResult.ViewName;
@@ -22,7 +24,7 @@ namespace HotelOrTaxi.Tests.Controllers
         [Test]
         public void ReturnsViewModel()
         {
-            var resultsController = new ResultsController();
+            var resultsController = new ResultsController(this);
             ViewResult viewResult = resultsController.Index(null, null, null, null);
 
             object model = viewResult.Model;
@@ -33,20 +35,21 @@ namespace HotelOrTaxi.Tests.Controllers
         [Test]
         public void ReturnsWinner()
         {
-            var resultsController = new ResultsController();
+            _taxiUri = "bob";
+            var resultsController = new ResultsController(this);
             ViewResult viewResult = resultsController.Index(null, null, null, null);
 
             var model = (ResultsViewModel) viewResult.Model;
 
             Assert.That(model.Winner.Name, Is.EqualTo("Taxi"));
             Assert.That(model.Winner.Price, Is.EqualTo("£26"));
-            Assert.That(model.Winner.Uri, Is.EqualTo("@Html.ActionLink(\"Show me\", \"Index\", \"Taxi\")"));
+            Assert.That(model.Winner.Uri, Is.EqualTo(_taxiUri));
         }
 
         [Test]
         public void ReturnsLoser()
         {
-            var resultsController = new ResultsController();
+            var resultsController = new ResultsController(this);
             ViewResult viewResult = resultsController.Index(null, null, null, null);
 
             var model = (ResultsViewModel) viewResult.Model;
@@ -54,8 +57,12 @@ namespace HotelOrTaxi.Tests.Controllers
             Assert.That(model.Loser.Name, Is.EqualTo("Hotel"));
             Assert.That(model.Loser.Price, Is.EqualTo("£30"));
             Assert.That(model.Loser.Uri,
-                        Is.EqualTo(
-                            "<a href =\"http://m.laterooms.com/en/p9827/MobileSearch.aspx?k=Manchester&d=20130404&n=1&adults=1&children=0&minp=&maxp=&StarRatingFilter=&rt=1-0&Latitude=&Longitude=&MaxRadius=1&PageSize=10&toStep=&SortBy=Price&Ascending=True&findButton=FIND+HOTELS&sb=tp&sd=true\">Show me</a></h3>"));
+                        Is.EqualTo("http://m.laterooms.com/en/p9827/MobileSearch.aspx?k=Manchester&d=20130404&n=1&adults=1&children=0&minp=&maxp=&StarRatingFilter=&rt=1-0&Latitude=&Longitude=&MaxRadius=1&PageSize=10&toStep=&SortBy=Price&Ascending=True&findButton=FIND+HOTELS&sb=tp&sd=true"));
+        }
+
+        public string GetUriForTaxi(UrlHelper url)
+        {
+            return _taxiUri;
         }
     }
 }
--- HotelOrTaxi/Controllers/ResultsController.cs
+++ HotelOrTaxi/Controllers/ResultsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Security.Policy;
+using System.Web.Mvc;
 using HotelOrTaxi.Models;
 
 
@@ -6,25 +7,58 @@ namespace HotelOrTaxi.Controllers
 {
     public class ResultsController : Controller
     {
+        private readonly ICreateTheTaxiControllerUri _createTheTaxiControllerUri;
+
+        public ResultsController(ICreateTheTaxiControllerUri createTheTaxiControllerUri)
+        {
+            _createTheTaxiControllerUri = createTheTaxiControllerUri;
+        }
+
         public ViewResult Index(string from, string to, string fromlatlong, string tolatlong)
         {
+            var winner = CreateWinner();
+
+            var loser = Result();
+
             var resultsViewModel = new ResultsViewModel();
-            Result winner = new Result
-                {
-                    Name = "Taxi",
-                    Price = "£26",
-                    Uri = "@Html.ActionLink(\"Show me\", \"Index\", \"Taxi\")"
-                };
-            resultsViewModel.Winner = winner;
-            Result loser = new Result
-                {
-                    Name = "Hotel",
-                    Price = "£30",
-                    Uri = "<a href =\"http://m.laterooms.com/en/p9827/MobileSearch.aspx?k=Manchester&d=20130404&n=1&adults=1&children=0&minp=&maxp=&StarRatingFilter=&rt=1-0&Latitude=&Longitude=&MaxRadius=1&PageSize=10&toStep=&SortBy=Price&Ascending=True&findButton=FIND+HOTELS&sb=tp&sd=true\">Show me</a></h3>"
-                };
             resultsViewModel.Loser = loser;
+            resultsViewModel.Winner = winner;
 
             return View("Index", resultsViewModel);
+        }
+
+        private static Result Result()
+        {
+            Result loser = new Result();
+            loser.Name = "Hotel";
+            loser.Price = "£30";
+            loser.Uri =
+                "http://m.laterooms.com/en/p9827/MobileSearch.aspx?k=Manchester&d=20130404&n=1&adults=1&children=0&minp=&maxp=&StarRatingFilter=&rt=1-0&Latitude=&Longitude=&MaxRadius=1&PageSize=10&toStep=&SortBy=Price&Ascending=True&findButton=FIND+HOTELS&sb=tp&sd=true";
+            return loser;
+        }
+
+        private Result CreateWinner()
+        {
+            Result winner = new Result();
+            winner.Name = "Taxi";
+            winner.Price = "£26";
+            winner.Uri = _createTheTaxiControllerUri.GetUriForTaxi(Url);
+            return winner;
+        }
+
+        
+    }
+
+    public interface ICreateTheTaxiControllerUri
+    {
+        string GetUriForTaxi(UrlHelper url);
+    }
+
+    public class CreateTheTaxiControllerUri : ICreateTheTaxiControllerUri
+    {
+        public string GetUriForTaxi(UrlHelper url)
+        {
+            return url.Action("Index", "Taxi");
         }
     }
 }
