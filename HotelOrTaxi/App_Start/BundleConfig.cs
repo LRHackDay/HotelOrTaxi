a1f81aa7a2c3e5a852fa668038bf5df9c7da7c89
Andrew Moorcroft <moorcroftlad+git@hotmail.com>
2013-04-04 08:53:12 +0100

    added jquery to bundler

--- HotelOrTaxi/App_Start/BundleConfig.cs
+++ HotelOrTaxi/App_Start/BundleConfig.cs
@@ -8,7 +8,8 @@ namespace HotelOrTaxi.App_Start
         {
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
                 "~/assets/js/bootstrap.js",
-                "~/assets/js/add2home.js"));
+                "~/assets/js/add2home.js",
+                "~/assets/js/jquery-1.9.1.js"));
             bundles.Add(new StyleBundle("~/bundles/css").Include(
                 "~/assets/css/bootstrap-responsive.css",
                 "~/assets/css/style.css",
