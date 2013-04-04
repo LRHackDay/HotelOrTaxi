75fbe11604887d2bbe9636e803f4f4ef94a134de
rubygem <gemma.cameron@gmail.com>
2013-04-04 13:04:56 +0100

    refactoring a new view model factory

--- HotelOrTaxi/Controllers/ResultsController.cs
+++ HotelOrTaxi/Controllers/ResultsController.cs
@@ -1,30 +1,30 @@
-﻿using System.Security.Policy;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using HotelOrTaxi.Models;
-
 
 namespace HotelOrTaxi.Controllers
 {
-    public class ResultsController : Controller
+    public interface ICreateResultViewModels
+    {
+        ResultsViewModel Create(UrlHelper urlHelper);
+    }
+
+    public class ResultsViewModelFactory : ICreateResultViewModels
     {
         private readonly ICreateTheTaxiControllerUri _createTheTaxiControllerUri;
 
-        public ResultsController(ICreateTheTaxiControllerUri createTheTaxiControllerUri)
+        public ResultsViewModelFactory(ICreateTheTaxiControllerUri createTheTaxiControllerUri)
         {
             _createTheTaxiControllerUri = createTheTaxiControllerUri;
         }
 
-        public ViewResult Index(string from, string to, string fromlatlong, string tolatlong)
+        public ResultsViewModel Create(UrlHelper urlHelper)
         {
-            var winner = CreateWinner();
-
+            var winner = CreateWinner(urlHelper);
             var loser = Result();
-
             var resultsViewModel = new ResultsViewModel();
             resultsViewModel.Loser = loser;
             resultsViewModel.Winner = winner;
-
-            return View("Index", resultsViewModel);
+            return resultsViewModel;
         }
 
         private static Result Result()
@@ -37,16 +37,35 @@ namespace HotelOrTaxi.Controllers
             return loser;
         }
 
-        private Result CreateWinner()
+        private Result CreateWinner(UrlHelper urlHelper)
         {
             Result winner = new Result();
             winner.Name = "Taxi";
             winner.Price = "£26";
-            winner.Uri = _createTheTaxiControllerUri.GetUriForTaxi(Url);
+            winner.Uri = _createTheTaxiControllerUri.GetUriForTaxi(urlHelper);
             return winner;
         }
+    }
 
-        
+
+    public class ResultsController : Controller
+    {
+        private readonly ICreateTheTaxiControllerUri _createTheTaxiControllerUri;
+        private readonly ResultsViewModelFactory _resultsViewModelFactory;
+
+        public ResultsController(ICreateTheTaxiControllerUri createTheTaxiControllerUri)
+        {
+            _createTheTaxiControllerUri = createTheTaxiControllerUri;
+            _resultsViewModelFactory = new ResultsViewModelFactory(_createTheTaxiControllerUri);
+        }
+
+        public ViewResult Index(string from, string to, string fromlatlong, string tolatlong)
+        {
+            var resultsViewModelFactory = _resultsViewModelFactory;
+            var resultsViewModel = resultsViewModelFactory.Create(Url);
+
+            return View("Index", resultsViewModel);
+        }
     }
 
     public interface ICreateTheTaxiControllerUri
