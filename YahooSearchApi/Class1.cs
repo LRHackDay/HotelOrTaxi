345d748f7205e7878bf5c9be31911bea296c3f1a
rubygem <gemma.cameron@gmail.com>
2013-04-06 18:26:14 +0100

    adding some stuff for the local search and some funky stuff on the from field to delete the text if you click on it. Andy, please don't shout at me.

--- YahooSearchApi/Class1.cs
+++ YahooSearchApi/Class1.cs
@@ -1,10 +1,46 @@
-﻿namespace YahooSearchApi
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Newtonsoft.Json;
+using WebResponse;
+
+namespace YahooSearchApi
 {
     /// <summary>
     /// http://developer.yahoo.com/search/local/V3/localSearch.html
-    /// http://local.yahooapis.com/LocalSearchService/V3/localSearch?appid=YahooDemo&query=pizza&zip=94306&results=2
     /// </summary>
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        [Ignore]
+        public void Bob()
+        {
+            var webClient = new WebClientWrapper();
+            string address =
+                "http://local.yahooapis.com/LocalSearchService/V3/localSearch?appid=yahoo_app_id&query=taxi&location=Manchester&results=3&output=json";
+
+            var response =
+                webClient.DownloadString(
+                    address);
+
+            YahooSearchResponse bob = JsonConvert.DeserializeObject<YahooSearchResponse>(response);
+            Assert.That(bob.ResultSet.Result[0].Phone, Is.EqualTo("Simon"));
+        }
+    }
+
+    public class YahooSearchResponse
+    {
+        public ResultSet ResultSet { get; set; }
+    }
+
+    public class ResultSet
+    {
+        public List<Result> Result { get; set; }
+    }
+
+    public class Result
+    {
+        public string Title { get; set; }
+        public string Phone { get; set; }
     }
 }
