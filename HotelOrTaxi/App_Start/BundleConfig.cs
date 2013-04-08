a86b126e3f869dd3d585eaa8dbe1528c71b7e1c6
Andrew Moorcroft <moorcroftlad+git@hotmail.com>
2013-04-08 15:51:03 +0100

    moved location.js to be used only on homepage

--- HotelOrTaxi/App_Start/BundleConfig.cs
+++ HotelOrTaxi/App_Start/BundleConfig.cs
@@ -9,8 +9,7 @@ namespace HotelOrTaxi.App_Start
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
                 "~/assets/js/bootstrap.js",
                 "~/assets/js/jquery-1.9.1.js",
-                "~/assets/js/autocomplete.js",
-                "~/assets/js/location.js"));
+                "~/assets/js/autocomplete.js"));
             bundles.Add(new StyleBundle("~/bundles/css").Include(
                 "~/assets/css/bootstrap-responsive.css",
                 "~/assets/css/style.css",
