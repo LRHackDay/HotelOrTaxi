80ca483de9577db4f4fd8bac9b0d6d294580c60e
Andrew Moorcroft <moorcroftlad+git@hotmail.com>
2013-04-09 11:21:36 +0100

    search form change location

--- HotelOrTaxi/assets/js/location.js
+++ HotelOrTaxi/assets/js/location.js
@@ -1,28 +1,3 @@
 ﻿$(document).ready(function () {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
-    } else {
-        alert('It seems like Geolocation, which is required for this page, is not enabled in your browser. Please use a browser which supports it.');
-    }
-
-    validateSearchForm();
+    
 });
-
-function successFunction(position) {
-    $('#fromlatlong').val(position.coords.latitude + ',' + position.coords.longitude);
-}
-
-function errorFunction(position) {
-       
-}
-
-function validateSearchForm() {
-    $('#fight-button').click(function (e) {
-        var from = $('#from').val(),
-            to = $('#to').val();
-        if (from.length === 0 || to.length === 0) {
-            e.preventDefault();
-            alert('Please enter your current destination and desired location');
-        }
-    });
-}
