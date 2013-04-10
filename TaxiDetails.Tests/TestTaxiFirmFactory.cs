c1b9ccf479235683d7ecbdd959932efbf0dddca2
rubygem <gemma.cameron@gmail.com>
2013-04-10 17:10:25 +0100

    reintroduced the taxi details tests

--- TaxiDetails.Tests/TestTaxiFirmFactory.cs
+++ TaxiDetails.Tests/TestTaxiFirmFactory.cs
@@ -15,7 +15,7 @@ namespace TaxiDetails.Tests
             var latitude = new Latitude("53.477716");
 
             var taxiFirmRepository = new TaxiFirmFactory(this, this);
-            Location location = new Location(latitude, longitude);
+            var location = new Location(latitude, longitude);
             List<TaxiFirm> taxiFirms = taxiFirmRepository.Create(location);
 
             Assert.That(taxiFirms[0].Name, Is.EqualTo("Manchester Cars"));
@@ -30,7 +30,7 @@ namespace TaxiDetails.Tests
 
         public string GetPlaceRequest(string placeReference)
         {
-            return null;
+            return "{\"result\" : {\"formatted_address\" : \"41 Bloom Street, Manchester, United Kingdom\",\"formatted_phone_number\" : \"0161 228 3355\",\"id\" : \"02ab79450269577e5afdca8490cb7a7996c922e4\",\"international_phone_number\" : \"+44 161 228 3355\",\"name\" : \"Manchester Cars\",\"reference\" : \"CnRtAAAAjnz16RphOCC0HPj6Uz4T3_UcehjYzZc9lx6K8609W8LTtC25xosND-C5aoy1czWRscDKKnLYcNiRs2vHnPzuK8Jq-S9nraXmQTxmZwwdDbl7Fc_SESVm3l6WdgjwOvHrN8t2ceo97U7d10htqGp-8xIQyOvdicRyiTStsq10taaK-hoUyz5KZFUQCfNLePav08TkERTYeCE\"}}";
         }
     }
 }
